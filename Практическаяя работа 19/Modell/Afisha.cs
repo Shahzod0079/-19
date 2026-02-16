28d0ac9aa6864fa2a0492fec71b2c1655bb3bdf4
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-02-16 19:45:27 +0500

    Создание классов. Тошматов

---
+++ Практическаяя работа 19/Modell/Afisha.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Практическая_19.Modell
+{
+    public class Afisha
+    {
+        public int Id { get; set; }
+        public int KinoteatrId { get; set; }
+        public string KinoteatrName { get; set; }
+        public string FilmName { get; set; }
+        public DateTime SessionTime { get; set; }
+        public decimal TicketPrice { get; set; }
+    }
+}
---
+++ Практическаяя работа 19/Modell/Kinoteatr.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Практическая_19.Modell
+{
+    public class Kinoteatr
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int HallCount { get; set; }
+        public int SeatCount { get; set; }
+    }
+}

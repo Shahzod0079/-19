824391be63c9593251b6febd22f8864543fa86c7
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-02-16 20:27:49 +0500

    Созданная страница списка кинотеатров. Тошматов

--- Практическаяя работа 19/Pages/Kinoteatr/Main.xaml.cs
+++ Практическаяя работа 19/Pages/Kinoteatr/Main.xaml.cs
@@ -1,28 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Практическая_19.Classes;
+using Практическая_19.Modell;
+using Практическая_19.Pages.Kinoteatr.Items;
 
-namespace Практическаяя_работа_19.Pages.Kinoteatr
+namespace Практическая_19.Pages.Kinoteatr
 {
-    /// <summary>
-    /// Логика взаимодействия для Main.xaml
-    /// </summary>
     public partial class Main : Page
     {
+        KinoteatrContext context;
+
         public Main()
         {
             InitializeComponent();
+            context = new KinoteatrContext();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            dgKinoteatrs.ItemsSource = context.GetAll();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Add());
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgKinoteatrs.SelectedItem is Kinoteatr kin)
+            {
+                NavigationService.Navigate(new Add(kin));
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgKinoteatrs.SelectedItem is Kinoteatr kin)
+            {
+                if (MessageBox.Show("Удалить?", "Подтверждение", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    context.Delete(kin.Id);
+                    LoadData();
+                }
+            }
         }
     }
 }
